14e537aa00c577a0415d5a7020c7051e4069308c
totomus83 <tom.dibellonio@gmail.com>
2025-07-07 17:31:02 +0200

    commited to full reference stle + added a comment

--- my-app/server/routes/newsletterRoutes.js
+++ my-app/server/routes/newsletterRoutes.js
@@ -1,10 +1,11 @@
+// sets up API endpoints for newsletter actions
+
 const express = require('express');
 const router = express.Router();
 const NewsletterController = require('../controllers/newsletterController');
 
-const { subscribe, getSubscribers } = NewsletterController;
 
-router.post('/subscribe', subscribe);
+router.post('/subscribe', NewsletterController.subscribe);
 router.get('/subscribers', NewsletterController.getSubscribers);
 router.delete('/unsubscribe', NewsletterController.unsubscribe);
 
