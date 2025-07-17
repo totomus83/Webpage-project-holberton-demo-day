296bc911eb206f9fec15d7e9471c652d5234f0e4
totomus83 <tom.dibellonio@gmail.com>
2025-07-17 14:28:55 +0200

    added a function to display images as a gallery

---
+++ my-app/client/effects/ImageGallery.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const images = [
+  "/client/images/puzzlerune2.png",
+  "/client/images/room1_view.png",
+  "/client/images/runeboat2.png",
+  "/client/images/BeachConifers.png",
+  "/client/images/BeachFireflyTree.png",
+  "/client/images/BeachForest.png",
+];
+
+export default function ImageGallery() {
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+
+  return (
+    <div className="gallery-container">
+      <div className="main-image-wrapper">
+        <img src={selectedImage} alt="Selected" className="main-image" />
+      </div>
+      <div className="thumbnail-row">
+        {images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt={`Thumbnail ${index}`}
+            className={`thumbnail ${selectedImage === img ? "active" : ""}`}
+            onClick={() => setSelectedImage(img)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
